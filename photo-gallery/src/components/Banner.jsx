b735d2c762343deaa702b4e2c6ca5b8a9398039d
Νίκος Καραμαρούδης <99655065+karamas13@users.noreply.github.com>
2024-10-17 22:41:46 +0300

    Added introductory logo image

--- photo-gallery/src/components/Banner.jsx
+++ photo-gallery/src/components/Banner.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css'; 
 import { useNavigate } from 'react-router';
 import logo3 from '/images/logo3.png';
+import perspective from '/images/perspective.png';
 
 const Banner = () => {
     const nav = useNavigate();
@@ -33,11 +34,11 @@ const Banner = () => {
             </div>
 
             <div className="flex flex-col justify-center items-center absolute left-1/2 transform -translate-x-1/2 w-full max-w-screen-lg h-screen max-h-screen-lg px-4">
-                <img className="w-[42rem] h-auto" src={logo3} />
+                <img className="lg:w-[40rem] md:w-[20rem] mb-[7rem] h-auto" src={perspective} />
             </div>
 
-               <div className="w-screen h-screen flex items-end justify-center ">
-               <button onClick={() => {nav('/HomePage')}} class="mb-20 text-2xl rounded-lg text-black hover:before:bg-black relative h-[50px] w-40 overflow-hidden border border-white bg-white px-3 text-black-900 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-500 hover:text-white hover:shadow-white hover:before:left-0 hover:before:w-full"><span class="relative z-10">Explore</span></button>
+               <div className="w-screen h-screen flex items-end justify-center mt-5">
+               <button onClick={() => {nav('/HomePage')}} class="mb-20 text-2xl rounded-lg text-black hover:before:bg-black relative h-[50px] w-40 overflow-hidden border border-white bg-white px-3 text-black-900 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-500 hover:text-white hover:shadow-white hover:before:left-0 hover:before:w-full"><span class="relative z-1">Explore</span></button>
             
             </div>
         </div>
