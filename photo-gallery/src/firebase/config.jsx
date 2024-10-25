347eb0fb7054e1df997f079af63e372fddef5fce
Νίκος Καραμαρούδης <99655065+karamas13@users.noreply.github.com>
2024-10-25 16:32:19 +0300

    Configure Environment Variables

--- photo-gallery/src/firebase/config.jsx
+++ photo-gallery/src/firebase/config.jsx
@@ -2,8 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes } from  'firebase/storage';
 import  { getFirestore, serverTimestamp } from 'firebase/firestore';
 
+
+const apiKey = `${import.meta.env.API_KEY}`;
+const authDomain = `${import.meta.env.AUTH_DOMAIN}`;
+const projectId = `${import.meta.env.PROJECT_ID}`;
+const storageBucket = `${import.meta.env.STORAGE_BUCKET}`;
+const messagingSenderId = `${import.meta.env.MESSAGING_SENDER_ID}`;
+const appId = `${import.meta.env.APP_ID}`;
+
 const firebaseConfig = {
-   
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId:"photo-gallery-59fc9",
+    storageBucket: "photo-gallery-59fc9.appspot.com",
+    messagingSenderId: "1049410031174",
+    appId: appId
   };
 
 // Initialize Firebase
--- photo-gallery/vite.config.js
+++ photo-gallery/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+    'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+    'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+    'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
+    'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
+    
+  }
 })
